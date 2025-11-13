6ec7ce4e2c51461a892c83f7cc0808ca1d4c20ed
Anashkin Artem <asanashkin@sberbank.ru>
2025-11-13 19:11:34 +0300

    Fix experimental material API
    Signed-off-by: Anashkin Artem <asanashkin@sberbank.ru>

--- composeApp/src/jvmMain/kotlin/com/bigdotdev/aospbugreportanalyzer/DesktopChatMain.kt
+++ composeApp/src/jvmMain/kotlin/com/bigdotdev/aospbugreportanalyzer/DesktopChatMain.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.bigdotdev.aospbugreportanalyzer
 
 import androidx.compose.foundation.background
@@ -23,6 +25,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
